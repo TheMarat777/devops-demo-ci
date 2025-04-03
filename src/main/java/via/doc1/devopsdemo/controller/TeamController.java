c10d6039d696ca028620cc6a7a04aff9d8be9167
Marat Colesnic <marat.colesnic@gmail.com>
2025-04-03 10:26:57 +0200

    Fix TeamController path mapping

--- src/main/java/via/doc1/devopsdemo/controller/TeamController.java
+++ src/main/java/via/doc1/devopsdemo/controller/TeamController.java
@@ -2,23 +2,24 @@ package via.doc1.devopsdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import via.doc1.devopsdemo.model.Task;
 import via.doc1.devopsdemo.service.TeamService;
 
 @RestController
+@RequestMapping("/members")
 public class TeamController {
 
-    private TeamService teamService;
+    private final TeamService teamService;
 
     public TeamController(TeamService teamService) {
         this.teamService = teamService;
     }
 
-    @GetMapping("/members/{memberId}/tasks/{taskId}")
+    @GetMapping("/{memberId}/tasks/{taskId}")
     public Task getTaskDetails(@PathVariable String memberId,
                                @PathVariable String taskId) {
         return teamService.getTask(memberId, taskId);
     }
 }
-
